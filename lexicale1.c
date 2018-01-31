3e6cd4d5cc9d5fecc0171e1394dae516f865e311
Vadym <33296686+r0shka@users.noreply.github.com>
2018-01-31 13:09:12 +0100

    lexicale v2
    old lexicale1.c

--- lexicale1.c
+++ lexicale1.c
@@ -3,20 +3,19 @@
 #include <string.h>
 #include <ctype.h>
 #include "symboles.h"
-#define Taille // variable globale dont la taille du tableau de caracteres et de noms
+#define TAILLE 16 // variable globale dont la taille du tableau de caracteres et de noms
 
 
 // fonction permettant d'ajouter un caractere et de le relier avec son nom si variable ou fonction
-void AddTableau( int tab[Taille]){
-	int tabnom[Taille]; // tableau de noms de variables et de fonctions codées soit par 26 soit par 27
-	int i, j=0;
-	tab = (int *) realloc(tab, 1024); // modification de la taille du tableau de caractere tout en conservant le contenu initial
-	for(i = 0; i< Taille ; i++){
+void AddTableau(int * tab){
+	//int i, j=0;
+	tab = (int *) realloc(tab, TAILLE * 2); // modification de la taille du tableau de caractere tout en conservant le contenu initial
+	/**for(i = 0; i< TAILLE ; i++){
 		if( tab[i] == 26 || tab[i] == 27 ){
 			tab[i] = tabnom[j];
 			j++;
 		}
-	}
+	}**/
 }
 
 /**set buffer array to 0**/
@@ -35,41 +34,93 @@ int c,
 char buffer[64];
 initBuffer(buffer);
 FILE *file;
-file = fopen("L.txt", "r");
+file = fopen("L", "r");
 if (file) {
     while ((c = getc(file)) != EOF){
 	    // appel à la fonction AddTbleau
-	    if(i < Taille ){
-			AddTableau(// j'ai un souci avec le type int du parametre??
-		}
+	    /**if(i > TAILLE-1 ){
+			AddTableau(tabID);
+		}**/
         if (isalpha(c)){
-			puts("it's a letter(to buffer)");
+			//puts("it's a letter(to buffer)");
 			buffer[i]=c;
 			i++;
 		}
 		else if (isdigit(c))
-			puts("ENTIER");
-		else if(c==32)
-			puts("SPACE");
-		else if(c==40){	//(
-			printf("%s\n", buffer );
-			puts("PARENTHESE_OUVRANTE");
-			i=0;
-			initBuffer(buffer);
+			puts("NOMBRE");
+		/**else if(c==32)
+			puts(" ");**/
+		else if(c==43){	//	+
+			if(buffer[0]!=0){
+				printf("%s\n", buffer );
+				i=0;
+				initBuffer(buffer);
+			}
+			puts("PLUS");
 		}
-		else if(c==41){  //)
-			printf("%s\n", buffer );
-			i=0;
-			initBuffer(buffer);
+		else if(c==61){	//	=
+			if(buffer[0]!=0){
+				printf("%s\n", buffer );
+				i=0;
+				initBuffer(buffer);
+			}
+			puts("EGALE");
+		}
+		else if(c==123){	//	{
+			if(buffer[0]!=0){
+				printf("%s\n", buffer );
+				i=0;
+				initBuffer(buffer);
+			}
+			puts("ACCOLADE_OUVRANTE");
+		}
+		else if(c==125){	//	}
+			if(buffer[0]!=0){
+				printf("%s\n", buffer );
+				i=0;
+				initBuffer(buffer);
+			}
+			puts("ACCOLADE_FERMANTE");
+		}
+		else if(c==59){	//	;
+			if(buffer[0]!=0){
+				printf("%s\n", buffer );
+
+				i=0;
+				initBuffer(buffer);
+			}
+			puts("POINT_VIRGULE");
+		}
+		else if(c==40){	//	(
+			if(buffer[0]!=0){
+				printf("b%s\n", buffer );
+				i=0;
+				initBuffer(buffer);
+			}
+			puts("PARENTHESE_OUVRANTE");
+		}
+		else if(c==41){  //	)
+			if(buffer[0]!=0){
+				printf("%s\n", buffer );
+				i=0;
+				initBuffer(buffer);
+			}
 			puts("PARENTHESE_FERMANTE");
 		}
-		else if(c==36){ //$
-			printf("%s\n", buffer );
-			i=0;
-			initBuffer(buffer);	
+		else if(c==36){ //	$
+			if(buffer[0]!=0){
+				if(!strcmp(buffer, "entier"))
+					puts("ENTIER");
+				if(!strcmp(buffer, "retour" ))
+					puts("RETOUR");
+				//printf("%s\n", buffer );
+				i=0;
+				initBuffer(buffer);	
+			}
+			puts("ID_VAR");
 		}	
-		else
-			puts("Unknown character");
+		/**else
+			puts(" ");**/
 	}
     fclose(file);
 }
